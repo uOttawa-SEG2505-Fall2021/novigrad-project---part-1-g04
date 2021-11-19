8663e72b6ad4b28eadd05b2d0202368e20b7328d
Phoenix Bastien <pbast074@uottawa.ca>
2021-11-19 12:00:19 -0500

    quelques modifications sur service page

--- app/src/main/java/com/example/servicenovigrad/ServicePage.java
+++ app/src/main/java/com/example/servicenovigrad/ServicePage.java
@@ -2,23 +2,48 @@ package com.example.servicenovigrad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class ServicePage extends AppCompatActivity {
 
+    ListView serviceListView;
     Button addServiceButton;
     Button goBackButton;
+
+    private Activity context;
+    List<Service> services;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_page);
 
+        serviceListView = findViewById(R.id.serviceListView);
         addServiceButton = findViewById(R.id.addServiceButton);
         goBackButton = findViewById(R.id.goBackButton);
+
+        // Array adapter
+        ArrayAdapter adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.activity_list_item,
+                (List<String>) serviceListView
+        );
+
+        serviceListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // ADD SOME SHIT HERE
+            }
+        });
 
         //GO TO ADD SERVICE PAGE AFTER CLICKING "ADD SERVICE"
         addServiceButton.setOnClickListener(new View.OnClickListener() {
@@ -36,5 +61,7 @@ public class ServicePage extends AppCompatActivity {
                 finish();
             }
         });
+
+
     }
 }
