b4f801d9b64a6b2c82db10a52dfd9983e5028b16
Nassim Tiraoui <ntira053@uottawa.ca>
2021-11-24 22:32:10 -0500

    complété delete branch

--- app/src/main/java/com/example/servicenovigrad/DeleteBranch.java
+++ app/src/main/java/com/example/servicenovigrad/DeleteBranch.java
@@ -1,14 +1,95 @@
 package com.example.servicenovigrad;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 public class DeleteBranch extends AppCompatActivity {
+
+    private Button confirmBtn, cancelBtn;
+    private EditText branchNameText, phoneNumberText;
+    private DatabaseReference databaseReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delete_branch);
+
+        confirmBtn = findViewById(R.id.delete_confirm_button);
+        cancelBtn = findViewById(R.id.delete_cancel_button);
+        branchNameText = findViewById(R.id.deleteBranchName_text);
+        phoneNumberText = findViewById(R.id.deletePhoneNumber_text);
+
+
+
+
+
+        confirmBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String branchName = branchNameText.getText().toString().trim();
+                String phoneNumber = phoneNumberText.getText().toString().trim();
+
+                if(!branchName.isEmpty() && !phoneNumber.isEmpty()) {
+                    databaseReference = FirebaseDatabase.getInstance().getReference("Branches");
+                    databaseReference.child(Objects.requireNonNull(branchName)).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DataSnapshot> task) {
+                            if(task.isSuccessful()) {
+                                if(Objects.requireNonNull(task.getResult().exists())) {
+                                    DataSnapshot dataSnapshot = task.getResult();
+                                    String confirmPhoneNumber = String.valueOf(dataSnapshot.child("phoneNumber").getValue());
+
+                                    if(confirmPhoneNumber.equals(phoneNumber)) {
+                                        Toast.makeText(DeleteBranch.this, "Deleted service successfully.", Toast.LENGTH_SHORT).show();
+                                        databaseReference.child(branchName).removeValue();
+                                        finish();
+                                    } else {
+                                        Toast.makeText(DeleteBranch.this, "Phone number doesn't exist.", Toast.LENGTH_SHORT).show();
+                                    }
+                                } else {
+                                    Toast.makeText(DeleteBranch.this, "Branch doesn't exist.", Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                Toast.makeText(DeleteBranch.this, "Failed to delete branch.", Toast.LENGTH_SHORT).show();
+
+                            }
+                        }
+                    });
+                } else if(branchName.isEmpty()){
+                    Toast.makeText(DeleteBranch.this, "Branch name is empty.", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(DeleteBranch.this, "Phone number is empty.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), WelcomePageEmployee.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
